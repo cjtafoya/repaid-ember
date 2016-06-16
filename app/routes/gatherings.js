2a1bb732489d0c527b5da66d296ae82159e42b38
RHOPRHH <rhoprhh@gmail.com>
2016-06-16 13:45:26 -0400

    and removed from gatherings

--- app/routes/gatherings.js
+++ app/routes/gatherings.js
@@ -4,9 +4,5 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('gathering')
   },
-  actions: {
-    save: function(gathering) {
-      gathering.save().then(this.transitionTo('gatherings'));
-    }
-  }
+
 });
