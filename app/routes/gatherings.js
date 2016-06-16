b599a6f415db3c90de8b48bb11ca6e37e3dba531
RHOPRHH <rhoprhh@gmail.com>
2016-06-16 13:33:30 -0400

    can create a new gathering now

--- app/routes/gatherings.js
+++ app/routes/gatherings.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('gathering')
+  },
+  actions: {
+    save: function(gathering) {
+      gathering.save().then(this.transitionTo('gatherings'));
+    }
   }
 });
