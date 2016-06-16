7c24e5e0a2ecd40370113a7ec5fe214b277f75f2
RHOPRHH <rhoprhh@gmail.com>
2016-06-16 14:15:25 -0400

    can now delete gatherings

--- app/routes/gatherings/gathering.js
+++ app/routes/gatherings/gathering.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete: function(gathering){
+      gathering.deleteRecord();
+      gathering.save();
+      this.transitionTo('gatherings');
+    }
+  }
 });
